a361dcf0271e117644141e68c2d1a487f7d45520
SimonArutunyan <simon.06.02.85@gmail.com>
2026-01-31 10:56:01 -0800

    Add files via upload

---
+++ Tile/script.js
@@ -0,0 +1,13 @@
+document.getElementById('emailBtn').addEventListener('click', () => {
+  window.location.href = 'mailto:info@eleganttiles.com';
+});
+
+// Добавляем класс loaded после полной загрузки страницы для запуска анимаций
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+});
+
+// Кнопка "Contact via Email"
+document.getElementById('emailBtn').addEventListener('click', () => {
+  window.location.href = 'mailto:info@eleganttiles.com';
+});
